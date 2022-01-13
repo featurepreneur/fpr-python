49ad960e865153967012b36302f24159a1d17dfe
Rajasree2004 <rajasree.selva@gmail.com>
2022-01-13 09:15:27 +0530

    modified test file

--- rajasree/test.py
+++ rajasree/test.py
@@ -7,11 +7,36 @@ Source:
 '''
 
 # Import necessary modules
- 
-
+import base64
+encoded_data = ""
 
 def startpy():
-    pass
+    encode("I am Rajasree")
+    decode(encoded_data)
+
+def encode(text):
+    
+
+    text_data = text
+    ascii_data = text_data.encode('ascii')
+    base64_byte_data = base64.b64encode(ascii_data)
+
+    encoded_data = base64_byte_data.decode('ascii')
+    print(encoded_data)
+    return encoded_data
+
+
+def decode(encoded_data):
+
+    encoded_data = encoded_data
+
+
+    base64_byte_data = encoded_data.encode('ascii')
+    ascii_data = base64.b64decode(base64_byte_data)
+
+    text_data = ascii_data.decode('ascii')
+    print(text_data)
+    return text_data
 
 
 if __name__ == '__main__':
