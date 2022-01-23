afa25c93d5d96de3b5bf6c1f00f54e916d016a37
Priyanka-Masha <pri6401@gmail.com>
2022-01-23 11:55:25 +0530

    Added os file

---
+++ priyanka/os-type.py
@@ -0,0 +1,25 @@
+'''
+Created on 
+Course work: 
+@author: raja
+Source:
+    
+'''
+
+#plotform module 
+
+import platform
+
+def os():
+    
+    
+    return platform.system()
+
+def startpy():
+    
+    print(os())
+
+if __name__ == '__main__':
+    
+    startpy()
+
