2b3128c76503ef786b97f13658ee39b501b40c26
elakia <vmelakia@gmail.com>
2022-01-23 11:56:51 +0530

    update elakia file

---
+++ elakia/app.py
@@ -0,0 +1,12 @@
+import bcrypt
+from eve import Eve
+
+from eve.auth import BasicAuth
+
+class MyBasicAuth(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource,
+                   method):
+        return username == 'admin' and password == 'secret'
+
+app = Eve(auth=MyBasicAuth)
+app.run()
