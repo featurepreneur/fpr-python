1269e4b11891acf7e980f713a3b56038a89cda3d
elakia <vmelakia@gmail.com>
2022-01-23 14:44:59 +0530

    task updated

---
+++ priyanka/whois_sample.py
@@ -0,0 +1,29 @@
+'''
+Created on 
+Course work: 
+@author: raja
+Source:
+    
+'''
+
+# Import necessary modules
+
+import whois
+
+def startpy():
+
+
+    print("Enter a word: ")
+
+    word = str(input())
+
+    count = 0
+    
+    for i in word:
+        if (i.isupper()) == True:
+            count = count + 1
+    
+    print(count)
+
+if __name__ == '__main__':
+    startpy()
